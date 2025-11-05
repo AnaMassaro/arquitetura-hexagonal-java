fd04be90835273d6aaaaddc9c3468dac73d8e14e
Ana Julia Massaro <anamassaro02@gmail.com>
2025-11-04 21:14:02 -0300

    feat: Criando o adapter de update

---
+++ src/main/java/com/massaro/hexagonal/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,24 @@
+package com.massaro.hexagonal.adapters.out;
+
+import com.massaro.hexagonal.adapters.out.repository.CustomerRepository;
+import com.massaro.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.massaro.hexagonal.application.core.domain.Customer;
+import com.massaro.hexagonal.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+
+}
