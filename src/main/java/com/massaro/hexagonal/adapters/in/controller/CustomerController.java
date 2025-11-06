5b7ebf8ab519ba5401e55ba767ee63d989aa91e1
Ana Julia Massaro <anamassaro02@gmail.com>
2025-11-05 22:52:54 -0300

    feat: Criando usecase, adapter e rota para deleção

--- src/main/java/com/massaro/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/massaro/hexagonal/adapters/in/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.massaro.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.massaro.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.massaro.hexagonal.adapters.in.controller.response.CustomerResponse;
 import com.massaro.hexagonal.application.core.domain.Customer;
+import com.massaro.hexagonal.application.ports.in.DeleteCustomerByIdInputPort;
 import com.massaro.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.massaro.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.massaro.hexagonal.application.ports.in.UpdateCustomerInputPort;
@@ -23,6 +24,9 @@ public class CustomerController {
 
     @Autowired
     private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private DeleteCustomerByIdInputPort deleteCustomerByIdInputPort;
 
     @Autowired
     private CustomerMapper customerMapper;
@@ -48,6 +52,12 @@ public class CustomerController {
         var customer = customerMapper.toCustomer(customerRequest);
         customer.setId(id);
         updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final String id) {
+        deleteCustomerByIdInputPort.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
